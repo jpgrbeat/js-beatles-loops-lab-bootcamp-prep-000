e5b786273d1a5c206116d017becfde4e4cb126d2
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-05-11 13:57:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay(musicians, instruments){
+  let band = [];
+  for(let i = 0; i < musicians.length ; i++){
+    band [i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  
+  return band;
+}
+
+function johnLennonFacts(facts){
+  
+  let i = 0;
+  
+  while(i < facts.length){
+    facts[i] = `${facts[i]}!!!`;
+    i++;
+  }
+  
+  return facts;
+}
+
